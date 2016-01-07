0b90279e046507f3ca7b238c1b00fb4912a5c686
lucasB <lucas975@hotmail.fr>
2016-01-07 16:51:11 +0100

    fin tp3 et ajout tp4

--- tous_les_tp/tp3/main.c
+++ tous_les_tp/tp3/main.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> //Pour le pow
 
 int main(int argc, char *argv[])
 {
+	int choix = 0;
+
+	do
+	{
+		int min = 0;
+		int max = 101;
+
+		int tour = 0;
+
+		printf("Joueur 1 !\n");
+		int first = 0;
+
+		do
+		{
+			printf("Donner une valeur entre 0 et 100\n");
+			scanf_s("%d", &first);
+
+		} while (first < min || first > max);
+		system("cls");
+
+
+		int trouve = max / 2;
+
+		tour++;
+
+		do
+		{
+			if (trouve < first)
+			{
+				min = trouve;
+				trouve = (min + max) / 2;
+			}
+			if (trouve > first)
+			{
+				max = trouve;
+				trouve = max / 2;
+			}
+			tour++;
+		} while (first != trouve);
+
+		printf("Le rez est %d\n", trouve);
+		printf("Le nombre de tour est : %d\n", tour);
+
+		printf("reco ? \n 1 = oui\n 2 = non\n");
+
+		scanf("%d", &choix);
+
+	} while (choix == 1);
+
 
 }
