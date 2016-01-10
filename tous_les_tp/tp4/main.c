fc52a2f2b7c764a3091fdec394911e0c53b61fc4
lucasB <lucas975@hotmail.fr>
2016-01-10 17:28:44 +0100

    fin tp 4

--- tous_les_tp/tp4/main.c
+++ tous_les_tp/tp4/main.c
@@ -3,5 +3,43 @@
 
 int main(int argc, char *argv[]){
 
+	int valeur;
+	int x = 0;
+	int y = 0;
+	int min = 0;
+	printf("Donnez une valeur en 1 et 10:\n");
+	scanf("%d", &valeur);
+
+	while (x < valeur)
+	{
+		y = 0;
+		x++;
+
+		/*test*/
+		while (y < x)
+		{
+			printf("*");
+			y++;
+		}
+		/*fintest*/
+		printf("\n");
+	}
+
+	while (x > min)
+	{
+		y = 0;
+		x--;
+
+		/*test*/
+		while (y < x)
+		{
+			printf("*");
+			y++;
+		}
+		/*fintest*/
+		printf("\n");
+	}
+
+	printf("X est egale a %d", x);
 	system("pause");
 }
